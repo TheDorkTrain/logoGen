bb9c63d324db455c04e53db14f384b2730944661
thedorktrain <mackemosabe@gmail.com>
2024-04-29 15:22:03 -0500

    added working tests to the Circle Triangle and Square classes

--- shapes.js
+++ shapes.js
@@ -2,13 +2,13 @@ const fs = require('fs');
 const logoFolder = './generated_logos/';
 
 class Logo {
-    constructor(answers) {
+    constructor(answers = {}) {
       this.text = answers.text;
       this.textColor = answers.textColor;
       this.fontType = answers.fontType;
       this.shape = answers.shape;
-      this.backgroundColor = answers.backgroundColor;
       this.borderColor = answers.borderColor;
+      this.backgroundColor = answers.backgroundColor;
     }
   }
   class Circle extends Logo {
@@ -57,7 +57,7 @@ class Logo {
       this.render = `<polygon points="150,10 225,50 225,135 150,175 75,135 75,50" stroke="${this.borderColor}" stroke-width="6" fill="${this.backgroundColor}" />`;
     }}
      Hexagon.prototype.imagewriter = function () {
-         let logoDesign = `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200"> ${then.render}
+         let logoDesign = `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200"> ${this.render}
          <text x="50%" y="50%" font-size="45" font-family="${this.fontType}" fill="${this.textColor}" text-anchor="middle" dominant-baseline="middle">${this.text}</text>
           </svg>`
         fs.writeFileSync(`${logoFolder}${this.text}_logo.svg`, logoDesign);
@@ -70,7 +70,7 @@ class Logo {
         this.render = `<polygon points="60,150 90,125 120,150 150,125 180,150 210,125 240,150 210,25 180,60 150,25 120,60 90,25 " stroke="${this.borderColor}" stroke-width="6" fill="${this.backgroundColor}" />`
     }}
      Winged.prototype.imagewriter = function () {
-         let logoDesign = `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200"> ${then.render}
+         let logoDesign = `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200"> ${this.render}
          <text x="50%" y="50%" font-size="45" font-family="${this.fontType}" fill="${this.textColor}" text-anchor="middle" dominant-baseline="middle">${this.text}</text>
           </svg>`
         fs.writeFileSync(`${logoFolder}${this.text}_logo.svg`, logoDesign);
--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -1,18 +1,34 @@
-const { Circle } = require("../shapes");
+const { Circle, Triangle, Square } = require("../shapes");
 
 describe("Circle", () => {
-  test("should render svg for a green circle element", () => {
-    const expectedSvg = `<circle cx="150" cy="100" stroke="${this.borderColor}" stroke-width="6" r="95" fill="green" />`;
-    const circle = new Circle();
-    circle.backgroundColor("green");
-    const actualSvg = circle.render();
-    expect(actualSvg).toEqual(expectedSvg);
+  test("should accept a new color params for background and color and render them", () => {
+    const color = 'blue';
+    const borderColor = 'green';
+    const circle = new Circle({backgroundColor:color, borderColor:borderColor});
+    expect(circle.render).toBe(`<circle cx="150" cy="100" r="95" stroke="${borderColor}" stroke-width="6" fill="${color}"/>`);
+
   });
-  test("should accept a fillColor param", () => {
-    const expectedSvg = '<circle cx="150" cy="100" r="95" fill="blue" />';
-    const circle = new Circle();
-    circle.backgroundColor("blue");
-    const actualSvg = circle.render();
-    expect(actualSvg).toEqual(expectedSvg);
+
+});
+
+describe("Triangle", () => {
+  test("should accept a new color params for background and color and render them", () => {
+    const color = 'blue';
+    const borderColor = 'green';
+    const triangle = new Triangle({backgroundColor:color, borderColor:borderColor});
+    expect(triangle.render).toBe(`<polygon points="50,50 250,50 150,175" stroke="${borderColor}" stroke-width="6" fill="${color}" />`);
+
   });
 });
+
+describe("Square", () => {
+  test("should accept a new color params for background and color and render them", () => {
+    const color = 'blue';
+    const borderColor = 'green';
+    const square = new Square({backgroundColor:color, borderColor:borderColor});
+    expect(square.render).toBe(`<rect width="100" height="100" x="100" y="50" stroke="${borderColor}" stroke-width="6" fill="${color}"/>`);
+
+  });
+
+})
+
